3f149a832a19711e3a2df56f8a04d469aeaad6ce
Keshav07Chauhan <keshav07chauhan@gmail.com>
2025-08-15 00:34:57 +0530

    debug...

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8000/api/v1",
+  baseURL: "https://whatsapp-clone-backend-xytk.onrender.com/api/v1" || "http://localhost:8000/api/v1",
   withCredentials: true
 });
 
--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,7 @@
 // src/socket.js
 import { io } from "socket.io-client";
 
-export const socket = io(`${import.meta.env.VITE_API_BASE_URL}`, {
+export const socket = io("https://whatsapp-clone-backend-xytk.onrender.com/api/v1", {
   autoConnect: false, // we'll connect manually in ChatPage
   withCredentials: true
 });

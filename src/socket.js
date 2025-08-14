5e2d433666fa00fd8937dc1b67eb19a808106892
Keshav07Chauhan <keshav07chauhan@gmail.com>
2025-08-14 21:33:16 +0530

    url updation

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,7 @@
 // src/socket.js
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:8000", {
+export const socket = io(`${Process.env.VITE_API_BASE_URL}`, {
   autoConnect: false, // we'll connect manually in ChatPage
   withCredentials: true
 });

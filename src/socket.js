0ac0dbc9212009c4c8bc96711bb4fd86deb8aa22
Keshav07Chauhan <keshav07chauhan@gmail.com>
2025-08-14 22:20:10 +0530

    debugging files

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,7 @@
 // src/socket.js
 import { io } from "socket.io-client";
 
-export const socket = io(`${Process.env.VITE_API_BASE_URL}`, {
+export const socket = io(`${import.meta.env.VITE_API_BASE_URL}`, {
   autoConnect: false, // we'll connect manually in ChatPage
   withCredentials: true
 });
